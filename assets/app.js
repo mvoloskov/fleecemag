fb53229a4a092f7aa09a88fbfe15d67bba31b17e
mvoloskov <hello@miloslav.website>
2023-10-15 16:16:26 +0400

    svgs inlined, proper dark/light theme

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,5 @@
 const root = document.documentElement
 const mediaDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-const toggleDarkTheme = document.querySelector('[data-toggle-dark-theme]')
 
 let savedTheme = window.localStorage.getItem('theme')
 if (!savedTheme) {
@@ -10,20 +9,24 @@ if (!savedTheme) {
 
 const darkMode = () => {
 	root.classList.add('dark')
-	toggleDarkTheme.innerText = 'Light theme'
 	window.localStorage.setItem('theme', 'dark')
 }
 
 const lightMode = () => {
 	root.classList.remove('dark')
-	toggleDarkTheme.innerText = 'Dark theme'
 	window.localStorage.setItem('theme', 'light')
 }
 
 if (savedTheme === 'dark') darkMode()
 else lightMode()
 
-toggleDarkTheme.onclick = () => {
+setTimeout(() => {
+	const root = document.documentElement
+	root.style.setProperty('--bg-transition', 'background var(--transition-duration)')
+	root.style.transition = 'var(--root-transition)'
+})
+
+window.toggleTheme = () => {
 	const isDark = root.classList.contains('dark')
 	if (isDark) lightMode()
 	else darkMode()
