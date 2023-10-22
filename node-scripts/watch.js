a2f3eaf3c99c0fe3a507f8219aae62cdbbfbe020
mvoloskov <hello@miloslav.website>
2023-10-22 11:12:28 +0400

    serve integrated

--- node-scripts/watch.js
+++ node-scripts/watch.js
@@ -1,5 +1,7 @@
 import { watch } from 'chokidar'
 import { generateArticles, generatePages, moveAssets } from './build.js'
+import serve from 'serve-handler'
+import { createServer } from 'http'
 
 const has = (string, substrings) => {
 	for (const substring of substrings) if (string.includes(substring)) return true
@@ -11,3 +13,13 @@ watcher.on('all', (_event, path) => {
 	if (has(path, ['articles', 'article.njk', 'articles.njk', 'page.njk', 'macros.njk'])) generateArticles()
 	if (has(path, ['auth.njk', 'index.njk', 'page.njk', 'macros.njk'])) generatePages()
 })
+
+const server = createServer(async (req, res) => {
+	await serve(req, res, {
+		public: 'public',
+		cleanUrls: true,
+		trailingSlash: false,
+	})
+})
+
+server.listen(3000, () => console.log('http://localhost:3000'))
