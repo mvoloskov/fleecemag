e9d9b0763c4719f3e6b36a40c87739c1b2881b04
mvoloskov <hello@miloslav.website>
2023-10-22 11:46:14 +0400

    ip, dead code removed

--- node-scripts/watch.js
+++ node-scripts/watch.js
@@ -1,7 +1,13 @@
 import { watch } from 'chokidar'
-import { generateArticles, generatePages, moveAssets } from './build.js'
 import serve from 'serve-handler'
 import { createServer } from 'http'
+import { networkInterfaces } from 'os'
+import { generateArticles, generatePages, moveAssets } from './build.js'
+
+const getIp = () => {
+	const interfaces = Object.values(networkInterfaces())
+	for (const x of interfaces) for (const { address } of x) if (address.startsWith('192')) return address
+}
 
 const has = (string, substrings) => {
 	for (const substring of substrings) if (string.includes(substring)) return true
@@ -14,12 +20,5 @@ watcher.on('all', (_event, path) => {
 	if (has(path, ['auth.njk', 'index.njk', 'page.njk', 'macros.njk'])) generatePages()
 })
 
-const server = createServer(async (req, res) => {
-	await serve(req, res, {
-		public: 'public',
-		cleanUrls: true,
-		trailingSlash: false,
-	})
-})
-
-server.listen(3000, () => console.log('http://localhost:3000'))
+const server = createServer((req, res) => serve(req, res, { public: 'public', cleanUrls: true }))
+server.listen(3000, () => console.log(new Date(), `http://localhost:3000, http://${getIp()}:3000`))
